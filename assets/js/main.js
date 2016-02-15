bf32bc7263d512e235073988c6705b354e53f96c
Kevin Schmitt <kevin.schmitt@mni.thm.de>
2016-02-15 01:27:34 +0100

    upload-15.02.2015

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,36 +34,26 @@ $(document).ready(function(){
 }); 
 
 
-// Returns true if the specified element has been scrolled into the viewport.
-function isElementInViewport(elem) {
+function isScrolledIntoView(elem)
+{
     var $elem = $(elem);
+    var $window = $(window);
 
-    // Get the scroll position of the page.
-    var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
-    var viewportTop = $(scrollElem).scrollTop();
-    var viewportBottom = viewportTop + $(window).height();
+    var docViewTop = $window.scrollTop();
+    var docViewBottom = docViewTop + $window.height();
 
-    // Get the position of the element on the page.
-    var elemTop = Math.round( $elem.offset().top );
+    var elemTop = $elem.offset().top;
     var elemBottom = elemTop + $elem.height();
 
-    return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
 }
 
-// Check if it's time to start the animation.
-function checkAnimation() {
-    var $elem = $('#about');
+$(window).scroll(function () {
+    $('.section-animate').each(function () {
+        if (isScrolledIntoView(this) === true) {
+            $(this).addClass('onview')
+        }
+    });
 
-    // If the animation has already been started
-    if ($elem.hasClass('onview')) return;
-
-    if (isElementInViewport($elem)) {
-        // Start the animation
-        $elem.addClass('onview');
-    }
-}
-
-// Capture scroll events
-$(window).scroll(function(){
-    checkAnimation();
 });
+
